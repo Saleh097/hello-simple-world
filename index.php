0fc98b89e31ba00e8b9d553bf12eaf8313226a43
saleh <salehdvp@gmail.com>
2024-09-04 11:40:18 +0330

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Simple World</title>
+</head>
+<body>
+    <h2> <?php echo "From PHP \n Hello World!"?> </h2>
+</body>
+</html>
